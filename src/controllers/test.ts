92ab2b859d405229e00533dcf091cb1f8ef171f3
Zach King <zacheking10@gmail.com>
2022-03-09 00:44:12 -0700

    chore: initial configuration complete, ready to start writing code

---
+++ src/controllers/test.ts
@@ -0,0 +1,2 @@
+export const test = 'testing modules...';
+// Consider changing these controller files to extend the class paradigm, this would enable controller files to behave like Angular services
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+let testing = 'testing.. nodemon....';
+console.log(testing);
+
+import { test } from './controllers/test';
+console.log(test);
